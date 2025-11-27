640738a945ac09037a2ba9d0e47bbb1685702559
rutul <rutulgajjar0203@gmail.com>
2025-11-27 19:36:38 +0530

    update code

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = "http://localhost:5000";
+// const API_BASE = import.meta.env.VITE_API_URL;
+
 
 export const api = {
   uploadResume: (file) => {
